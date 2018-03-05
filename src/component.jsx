92fe5948483a1a9db9d5baf12c6d131e06c7610f
Renato Martins <renatto.martins@gmail.com>
2018-03-05 07:42:18 -0300

    Exporting multiples components - Syntax 2

--- src/component.jsx
+++ src/component.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const First = props => (
+export const First = props => (
     <h1>First component</h1>
 )
 
-const Second = props => (
+export const Second = props => (
     <h1>Second component</h1>
 )
-
-export {First, Second}
