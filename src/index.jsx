58f7c231029da268061e0f093d3b8657fa0d3354
Renato Martins <renatto.martins@gmail.com>
2018-11-19 10:29:27 -0200

    Removes field files to start another example

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
-import Field from './field'
-import fieldReducer from './fieldReducer'
 
 const reducers = combineReducers({
-    field: fieldReducer
+    nothing: "temp"
 })
 
 ReactDOM.render(
     <Provider store={createStore(reducers)}>
-        <Field initialValue='Teste' />
+        <div>Ok</div>
     </Provider>
 , document.getElementById('app'))
